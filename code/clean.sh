77054f5e0c38b680d2a1412c7be7119ad150ac11
Joseph Meleshko <jmeleshk@ualberta.ca>
2019-11-28 22:27:38 -0700

    properly escaping my newlines

--- code/clean.sh
+++ code/clean.sh
@@ -3,7 +3,7 @@ rm -rf ./output/*
 rm -rf ./mask/*
 rm -rf ./temp/*
 
-echo "* \n !.gitignore" > ./input/.gitignore
-echo "* \n !.gitignore" > ./output/.gitignore
-echo "* \n !.gitignore" > ./mask/.gitignore
-echo "* \n !.gitignore" > ./temp/.gitignore
+echo $'* \n !.gitignore' > ./input/.gitignore
+echo $'* \n !.gitignore' > ./output/.gitignore
+echo $'* \n !.gitignore' > ./mask/.gitignore
+echo $'* \n !.gitignore' > ./temp/.gitignore
